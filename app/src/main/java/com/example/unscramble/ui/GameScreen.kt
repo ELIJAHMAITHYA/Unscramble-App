af6e818d012e1ea96a72f933de19dc2bcd83869a
Sila Elijah <elimaithya21@gmail.com>
2023-09-20 15:52:35 +0300

    Implement the skip functionality

--- app/src/main/java/com/example/unscramble/ui/GameScreen.kt
+++ app/src/main/java/com/example/unscramble/ui/GameScreen.kt
@@ -88,7 +88,7 @@ fun GameScreen(
             }
 
             OutlinedButton(
-                onClick = { },
+                onClick = { gameViewModel.skipWord() },
                 modifier = Modifier.fillMaxWidth()
             ) {
                 Text(
--- app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
+++ app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -80,5 +80,10 @@ class GameViewModel : ViewModel() {
             )
         }
     }
+    fun skipWord() {
+        updateGameState(_uiState.value.score)
+        // Reset user guess
+        updateUserGuess("")
+    }
     }
 
