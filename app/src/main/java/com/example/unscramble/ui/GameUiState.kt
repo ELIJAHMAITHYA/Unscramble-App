c2f77f377d14c5df0d15653444e504302cb27346
Sila Elijah <elimaithya21@gmail.com>
2023-09-20 16:13:58 +0300

    Add logic to end the game

--- app/src/main/java/com/example/unscramble/ui/GameUiState.kt
+++ app/src/main/java/com/example/unscramble/ui/GameUiState.kt
@@ -5,4 +5,5 @@ package com.example.unscramble.ui
      val isGuessedWordWrong: Boolean = false,
      val score: Int = 0,
      val currentWordCount: Int = 1,
+     val isGameOver: Boolean= false
          )
