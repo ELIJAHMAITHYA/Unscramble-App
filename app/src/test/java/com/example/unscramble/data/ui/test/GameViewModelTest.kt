561ea67cda1b60169c3ab46a69272c1191ae4522
Sila Elijah <elimaithya21@gmail.com>
2023-09-21 10:50:16 +0300

    Error Path:Negative Flow

--- app/src/test/java/com/example/unscramble/data/ui/test/GameViewModelTest.kt
+++ app/src/test/java/com/example/unscramble/data/ui/test/GameViewModelTest.kt
@@ -5,11 +5,13 @@ import com.example.unscramble.data.getUnscrambledWord
 import com.example.unscramble.ui.GameViewModel
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class GameViewModelTest {
     private val viewModel = GameViewModel()
 
+    //Success Test
     @Test
     fun gameViewModel_CorrectWordGuessed_ScoreUpdatedAndErrorFlagUnset() {
         var currentGameUiState = viewModel.uiState.value
@@ -22,6 +24,23 @@ class GameViewModelTest {
         // Assert that score is updated correctly.
         assertEquals(20, currentGameUiState.score)
     }
+
+    //Error test
+    @Test
+    fun gameViewModel_IncorrectGuess_ErrorFlagSet() {
+        // Given an incorrect word as input
+        val incorrectPlayerWord = "and"
+
+        viewModel.updateUserGuess(incorrectPlayerWord)
+        viewModel.checkUserGuess()
+
+        val currentGameUiState = viewModel.uiState.value
+        // Assert that score is unchanged
+        assertEquals(0, currentGameUiState.score)
+        // Assert that checkUserGuess() method updates isGuessedWordWrong correctly
+        assertTrue(currentGameUiState.isGuessedWordWrong)
+    }
+
     companion object {
         private const val SCORE_AFTER_FIRST_CORRECT_ANSWER = SCORE_INCREASE
     }
