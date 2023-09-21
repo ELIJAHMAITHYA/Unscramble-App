8c44b01be6cb6b9ca9c43b53c592c20dd0e3d396
Sila Elijah <elimaithya21@gmail.com>
2023-09-21 11:02:34 +0300

    Boundary case:Testing

--- app/src/test/java/com/example/unscramble/data/ui/test/GameViewModelTest.kt
+++ app/src/test/java/com/example/unscramble/data/ui/test/GameViewModelTest.kt
@@ -5,6 +5,7 @@ import com.example.unscramble.data.getUnscrambledWord
 import com.example.unscramble.ui.GameViewModel
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
+import org.junit.Assert.assertNotEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
 
@@ -39,6 +40,30 @@ class GameViewModelTest {
         assertEquals(0, currentGameUiState.score)
         // Assert that checkUserGuess() method updates isGuessedWordWrong correctly
         assertTrue(currentGameUiState.isGuessedWordWrong)
+    }
+
+//    boundary Case
+    /*   To test the initial state of the UI, you need to write a unit test for the GameViewModel class. The test must assert that when the GameViewModel is initialized, then the following is true:
+
+       currentWordCount property is set to 1.
+       score property is set to 0.
+       isGuessedWordWrong property is set to false.
+       isGameOver property is set to false. */
+
+    @Test
+    fun gameViewModel_Initialisation_FirstWordLoaded() {
+        val gameUiState = viewModel.uiState.value
+        val unScrambledWord = getUnscrambledWord(gameUiState.currentScrambledWord)
+        // Assert that current word is scrambled.
+        assertNotEquals(unScrambledWord, gameUiState.currentScrambledWord)
+        // Assert that current word count is set to 1.
+        assertTrue(gameUiState.currentWordCount == 1)
+        // Assert that initially the score is 0.
+        assertTrue(gameUiState.score == 0)
+        // Assert that the wrong word guessed is false.
+        assertFalse(gameUiState.isGuessedWordWrong)
+        // Assert that game is not over.
+        assertFalse(gameUiState.isGameOver)
     }
 
     companion object {
